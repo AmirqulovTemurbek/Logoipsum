ec061fd093a5a8fedb38fda6ed0a5b0784b85095
AmirqulovTemurbek <amirqulovtemurbek@gmail.com>
2023-01-12 23:59:06 +0500

    read more...

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+const parentIndex = document.querySelector('.index__one');
+parentIndex.addEventListener('click', event => {
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read_more_btn');
+
+    if (!isReadMoreBtn) return;
+
+    const currentText= event.target.parentNode.querySelector('.read_more_text');
+
+    currentText.classList.toggle('read_more_text_show');
+
+    current.textContent = current.textContent.includes('Read More') ?
+    "Read Less..." : "Read More...";
+})
